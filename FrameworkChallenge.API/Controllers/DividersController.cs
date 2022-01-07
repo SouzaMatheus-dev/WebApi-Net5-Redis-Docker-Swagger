b29f3b86080f8ef8097307a806ee5b0ba1862a06
Matheus Neves <souzamath63@gmail.com>
2022-01-06 23:15:36 -0300

    Initial Commit

---
+++ FrameworkChallenge.API/Controllers/DividersController.cs
@@ -0,0 +1,41 @@
+﻿using FrameworkChallenge.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace FrameworkChallengeNet5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DividersController : Controller
+    {
+        private readonly ILogger<DividersController> _logger;
+
+        private readonly IDistributedCache _cache;
+
+        public DividersController(ILogger<DividersController> logger, IDistributedCache cache)
+        {
+            _logger = logger;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public DividersDTO Get(int number)
+        {
+            try
+            {
+                CalculatorDividers _calculate = new();
+                var calculateDivider = _calculate.Calculate(number);
+                _cache.SetString("_" + number, "Result" + calculateDivider);
+
+                return calculateDivider;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ FrameworkChallenge.Domain/CalculatorDividers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkChallenge.Domain
+{
+    public class CalculatorDividers
+    {
+        public DividersDTO Calculate(int Number)
+        {
+            DividersDTO dividers = new DividersDTO();
+            dividers.NumberDividers = new List<int>();
+            dividers.NumberDividersPrime = new List<int>();
+
+            for (int i = 1; i <= Number; i++)
+            {
+                if (Number % i == 0)
+                {
+                    dividers.NumberDividers.Add(i);
+                }
+            }
+
+            for (int y = 0; y < dividers.NumberDividers.Count; y++)
+            {
+                int cont = 0;
+                for (int i = dividers.NumberDividers[y]; i > 0; i--)
+                {
+                    if (dividers.NumberDividers[y] % i == 0)
+                    {
+                        cont++;
+                    }
+                }
+                if (cont == 2)
+                {
+                    dividers.NumberDividersPrime.Add(dividers.NumberDividers[y]);
+                }
+            }
+
+            return dividers;
+        }
+    }
+}
